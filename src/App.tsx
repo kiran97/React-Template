27a8c366c08455f378b4e767b62116f31e0aacd7
kiran97 <kirankumar.n.gowda97@gmail.com>
2021-08-19 11:43:39 +0530

    Initial dev setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
-export const App = () => <h1>React template </h1>
+import './styles.css'
+import IMAGE from './react.png'
+
+export const App = () => {
+  const num = 100
+  const test = "mfmgnfm"
+
+  return (
+    <>
+      <img src={IMAGE} alt="react logo" />
+      <h1>
+        React template new build running - {process.env.NODE_ENV} -{' '}
+        {process.env.name}
+      </h1>
+    </>
+  )
+}
